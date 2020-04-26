f49896e42db5eb836cf65d3f4c2ca02bd43bb3af
Joanna Lin <lin.joanna.y@gmail.com>
2020-04-26 16:32:28 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+Genre.destroy_all
+
+beyonce = Artist.create(name: "Beyonce", bio: "the most queeny bee i ever did see")
+one_republic = Artist.create(name: "One Republic", bio: "there can be only one")
+
+love= Song.create(name: "Love on Top")
+secrets= Song.create(name: "Secrets")
+
+rnb = Genre.create(name: "R&B")
+alternative = Genre.create(name: "Alternative")
